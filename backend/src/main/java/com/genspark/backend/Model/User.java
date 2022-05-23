73608e3289e1a5552649be402755d72d60ad60bc
rayalva407 <rayalva407@gmail.com>
2022-05-23 12:46:36 -0600

    Creates User entity class

---
+++ backend/src/main/java/com/genspark/backend/Model/User.java
@@ -0,0 +1,39 @@
+package com.genspark.backend.Model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
